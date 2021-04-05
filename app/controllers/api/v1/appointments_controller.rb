033c6b8e4ead54d22f75cbb7bf937f7c325d3b4d
reyes2981 <noe.reyes92@yahoo.com>
2021-04-05 00:35:49 -0500

    added index and create actions in api controller

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::AppointmentsController < ApplicationController # <- reflects namespace of route
+
+    def index
+        appointments = Appointment.all
+        render json: appointments
+    end
+
+    def create
+        appointment = Appointment.new(appointment_params)
+        if appointment.save
+            render json: appointment, status: :accepted
+        else
+            render json: {errors: appointment.errors.full_messages), status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:customer_id, :hairdresser_id, :datetime)
+    end
+
 end
