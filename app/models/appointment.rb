8da560ab69a8fb27261c089cc9041f27bdfe070d
reyes2981 <noe.reyes92@yahoo.com>
2021-04-23 01:25:06 -0500

    removed mobiscroll css and added a customers controller to api/v1

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :customer, optional: true # what is optional: true?
 
-  validates :first_name, presence: true
+  validates :first_name, :last_name, presence: true
 
 end
